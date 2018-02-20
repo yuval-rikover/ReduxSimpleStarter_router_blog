face52efaf0d3accbc7b6b24ce531e0a195d5492
yuval-rikover <yuval.rikover@kenshoo.com>
2018-02-20 16:32:09 +0200

    aaa

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -9,7 +10,11 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    <Provider store={createStoreWithMiddleware(reducers)}>
+        <div>
+            <BrowserRouter>
+                <Rout/>
+            </BrowserRouter>
+        </div>
+    </Provider>
+    , document.querySelector('.container'));
