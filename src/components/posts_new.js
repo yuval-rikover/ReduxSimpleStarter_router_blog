aa8e82335218bca071707b6ff7940f998aa1b83c
yuval-rikover <yuval.rikover@kenshoo.com>
2018-02-21 11:53:51 +0200

    zzzz

---
+++ src/components/posts_new.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class PostsNew extends Component {
+
+    render() {
+        return (
+            <div>
+                New post
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
